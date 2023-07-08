fa33e7070414529b8250c8879f5af5fe85ec1f39
Mustafa_Rizwan <123109871+runtime-error786@users.noreply.github.com>
2023-07-08 19:49:11 +0500

    Add files via upload

---
+++ project 3 js/java.js
@@ -0,0 +1,75 @@
+
+let f = document.getElementById("s2");
+f.addEventListener( "click" , function(){document.getElementById("s3").style.display="block";
+f.innerHTML = "what is the return policy&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-";
+let f10 = document.getElementById("s4");
+f10.innerHTML = "when will i receive my order&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+";
+
+let f11 = document.getElementById("s6");
+f11.innerHTML = " where are you located&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+";
+
+document.getElementById("s5").style.display="none";
+document.getElementById("s7").style.display="none";
+console.log("hello "); } );
+
+
+
+let f3 = document.getElementById("s2");
+f3.addEventListener( "dblclick" , function(){document.getElementById("s3").style.display="none";
+f.innerHTML = "what is the return policy&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+"
+document.getElementById("s5").style.display="none";
+document.getElementById("s7").style.display="none";
+console.log("hello "); } );
+
+
+
+//
+let f1 = document.getElementById("s4");
+f1.addEventListener( "click" , function(){document.getElementById("s5").style.display="block";
+f1.innerHTML = "when will i receive my order&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-";
+
+let f11 = document.getElementById("s6");
+f11.innerHTML = " where are you located&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+";
+
+let f12 = document.getElementById("s2");
+f12.innerHTML = "what is the return policy&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+";
+
+document.getElementById("s3").style.display="none";
+document.getElementById("s7").style.display="none";
+console.log("hello "); } );
+
+
+
+let f4 = document.getElementById("s4");
+f4.addEventListener( "dblclick" , function(){document.getElementById("s3").style.display="none";
+f4.innerHTML = "when will i receive my order&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+"
+document.getElementById("s5").style.display="none";
+document.getElementById("s7").style.display="none";
+console.log("hello "); } );
+
+
+
+let f2 = document.getElementById("s6");
+f2.addEventListener( "click" , function(){document.getElementById("s7").style.display="block";
+f2.innerHTML = " where are you located&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-";
+document.getElementById("s5").style.display="none";
+document.getElementById("s3").style.display="none";
+console.log("hello "); } );
+
+
+
+let f5 = document.getElementById("s6");
+f5.addEventListener( "dblclick" , function(){document.getElementById("s7").style.display="none";
+f5.innerHTML = " where are you located&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+"
+
+let f10 = document.getElementById("s4");
+f10.innerHTML = "when will i receive my order&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+";
+
+let f12 = document.getElementById("s2");
+f12.innerHTML = "what is the return policy&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+";
+
+document.getElementById("s3").style.display="none";
+document.getElementById("s5").style.display="none";
+console.log("hello "); } );
+
+
